51a6a866c5db3423aaa009bacce2bc020d2541d7
guilhermejulio <guilhermejuliocode@gmail.com>
2021-03-14 23:42:12 -0300

    Utilizando metodos HTTP

--- Chapter-I/fundamentos-nodejs/src/index.js
+++ Chapter-I/fundamentos-nodejs/src/index.js
@@ -3,13 +3,27 @@ const express = require('express');
 
 const app = express ();
 
-app.get('/', (request, response) => {
-  return response.json({
-    messsage: 'Helo World Ignite!'
-  });
-})
+app.get('/courses', (request, response) => {
+  return response.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
+
+app.post('/courses',(request, response) => {
+  return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.put('/courses/:id',(request, response) => {
+  return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.patch('/courses/:id',(request, response) => {
+  return response.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
+});
+
+app.delete('/courses/:id',(request, response) => {
+  return response.json(["Curso 6", "Curso 7", "Curso 4"]);
+});
+
 
 app.listen(2398, () => {
   console.log('Server start on port 2398! 🚀')
-}
-);
+});

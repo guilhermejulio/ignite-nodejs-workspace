72b03b1acdecc7b779dff1a28dc3d67f02a9cc0e
guilhermejulio <guilhermejuliocode@gmail.com>
2021-03-18 12:57:26 -0300

    Cadastro de conta

--- Chapter-I/02-primeiro-projeto-nodejs/src/index.js
+++ Chapter-I/02-primeiro-projeto-nodejs/src/index.js
@@ -1,9 +1,32 @@
 const { request, response } = require('express');
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express ();
-
 app.use(express.json());
+
+const customers = [];
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement - como sendo  extrato (creditos e debitos, lançamentos) - []
+ */
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body;
+  const id = uuidv4();
+
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: []
+  });
+
+  return response.send(201).send()
+
+});
+
 
 app.listen(2398, () => {
   console.log('Server start on port 2398! 🚀')
